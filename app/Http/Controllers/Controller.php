b8755a1f2d92708218af4bce2a8e90ee9c46ea13
achrafnabaha1 <achrafnabaha1@gmail.com>
2025-04-26 15:16:55 +0100

    Fix espaces inutiles et fin de fichier

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    // Ajout d'un espace inutile à la fin de cette ligne
+
+    // Exemple d'erreur : espace inutile à la fin du fichier
+
 }
