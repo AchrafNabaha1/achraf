9e492d2b6894cdfab9e9bd605975a7b60b34cc07
achrafnabaha1 <achrafnabaha1@gmail.com>
2025-04-26 15:23:08 +0100

    Test de pre-commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,23 @@
-<?php
+<?phpnamespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
-
-abstract class Controller
+abstract class controller
 {
     // Ajout d'un espace inutile à la fin de cette ligne
+    // Ce commentaire est trop long et n'apporte rien de pertinent, il devrait être supprimé ou raccourci
+
+    public function exampleMethod
+    {
+        // Erreur : pas de type de retour défini pour la méthode
+        $var = 'test';
+    }
+
+    // Exemple d'indentation incorrecte avec des espaces
+    public function anotherMethod() {
+        $result = 'This should be indented with tabs, not spaces';
+    }
+
+    // Utilisation inutile de "use" qui n'est pas utilisé dans ce fichier
+    use App\Models\User;
 
     // Exemple d'erreur : espace inutile à la fin du fichier
 
